605ceef69099144d6d23e0a53b7e127a875ff865
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-07-28 10:41:15 -0300

    Card post on variable

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let userPostElement = `
+
+<div class="card-publication">
+
+<div class="information">
+
+    <div class="user-information">
+        <img src="/assets/user.svg" alt="">
+
+        <div>
+            <p>Diógenes Ribeiro</p>
+            <small>@diogenes_oliveira</small>
+        </div>
+
+    </div>
+
+    <h4> + Seguir </h4>
+</div>
+
+</div>
+
+`

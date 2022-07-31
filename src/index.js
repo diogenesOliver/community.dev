500b57d3279c45a62da565085512beb08ecce876
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-07-31 17:48:07 -0300

    Logic for openMenu and closeMenu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+let buttonOpenMenu = document.getElementById('openMenu')
+let buttonCloseMenu = document.getElementById('closeMenu')
+
+function openMenu(){
+
+    buttonOpenMenu.style.display = 'none'
+    buttonCloseMenu.style.display = 'block'
+
+    document.body.classList.add('menu-expanded')
+
+}
+
+function closeMenu(){
+
+    buttonCloseMenu.style.display = 'none'
+    buttonOpenMenu.style.display = 'block'
+
+    document.body.classList.remove('menu-expanded')
+
+}
+
+buttonOpenMenu.addEventListener('click', () => {
+    openMenu()
+})
+
+buttonCloseMenu.addEventListener('click', () => {
+    closeMenu()
+})
+
 let userPostElement = `
 
 <div class="puclications">

51ee275de7aaefede4d31215c72e56b1864d424b
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-04 11:47:16 -0300

    Creating the structure

--- src/base/community-card.js
+++ src/base/community-card.js
@@ -1,1 +1,25 @@
 let creatingCommunity = document.getElementById('createCommunity')
+
+let listOfCommunities = []
+
+function openModal(){
+
+}
+
+function closeModal(){
+
+}
+
+function addNewInformations(){
+
+    let objectInformation = {
+
+    }
+
+    listOfCommunities.push(objectInformation)
+
+}
+
+creatingCommunity.addEventListener('click', () => {
+    openModal()
+})

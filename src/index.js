5c741392afc49afc576fc0cb4e430aaf30d0c845
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-04 11:59:10 -0300

    Removing

--- src/index.js
+++ src/index.js
@@ -22,29 +22,3 @@ let typedArticles = new Typed(numberArticles, {
     backSpeed: 90,
     loop: true
 })
-
-let userPostElement = `
-
-<div class="puclications">
-
-        <div class="head-publication">
-
-            <div class="infromations">
-
-                <img src="/assets/user.svg" alt="">
-                <div>
-                    <h4> Diógenes Ribeiro </h4>
-                    <small> @diogenes_oliveira </small>
-                </div>
-
-            </div>
-
-            <button> + Seguir </button>
-
-        </div>
-
-        <p>  </p>
-
-    </div>
-
-`

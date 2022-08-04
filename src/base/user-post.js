1855247140ce711b740347be3a25d97895e29e04
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-04 11:58:35 -0300

    User post element HTML

---
+++ src/base/user-post.js
@@ -0,0 +1,25 @@
+let userPostElement = `
+
+<div class="puclications">
+
+        <div class="head-publication">
+
+            <div class="infromations">
+
+                <img src="/assets/user.svg" alt="">
+                <div>
+                    <h4> Diógenes Ribeiro </h4>
+                    <small> @diogenes_oliveira </small>
+                </div>
+
+            </div>
+
+            <button> + Seguir </button>
+
+        </div>
+
+        <p>  </p>
+
+    </div>
+
+`

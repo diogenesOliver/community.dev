076925e41f3e412389ff37a4445ccc5affd80cbc
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-05 13:32:26 -0300

    Logic to create a community

--- src/base/community-card.js
+++ src/base/community-card.js
@@ -1,16 +1,62 @@
 let creatingCommunity = document.getElementById('createCommunity')
+let communityModal = document.querySelector('.modal-community')
+let closeModalButton = document.getElementById('closeModalCommunity')
+
+let createButton = document.getElementById('createButton')
 
 let listOfCommunities = []
 
-function openModal(){
-
+function openModal() {
+    communityModal.style.display = 'flex'
 }
 
-function closeModal(){
-
+function closeModal() {
+    communityModal.style.display = 'none'
 }
 
-function addNewInformations(){
+function addNewInformations() {
+
+    let communityTitle = document.getElementById('communityTitle').value
+    let communityDescription = document.getElementById('communityDescription').value
+
+    let tagOne = document.getElementById('tag1').value
+    let tagTwo = document.getElementById('tag2').value
+    let tagThree = document.getElementById('tag3').value
+
+    let createCommunityElement = `
+    <div class="card-community">
+
+        <div class="header-card">
+            <h1>${communityTitle}</h1>
+            <h4> 10.356 users </h4>
+        </div>
+
+        <div class="description">
+            <p> ${communityDescription} </p>
+        </div>
+
+        <div class="user-image-perfil">
+            <img src="/assets/user.svg">
+            <img src="/assets/user.svg">
+            <img src="/assets/user.svg">
+        </div>
+
+        <div class="tags">
+
+            <ul>
+                <li> <a href=""> ${tagOne} </a> </li>
+                <li> <a href=""> ${tagTwo} </a> </li>
+                <li> <a href=""> ${tagThree} </a> </li>
+            </ul>
+
+        </div>
+
+        <button> Get started </button>
+
+    </div>
+`
+
+    document.querySelector('.cards').innerHTML += createCommunityElement
 
     let objectInformation = {
 
@@ -22,4 +68,13 @@ function addNewInformations(){
 
 creatingCommunity.addEventListener('click', () => {
     openModal()
+})
+
+closeModalButton.addEventListener('click', () => {
+    closeModal()
+})
+
+createButton.addEventListener('click', () => {
+    addNewInformations()
+    closeModal()
 })

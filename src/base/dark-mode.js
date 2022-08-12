ded27d74f1d2a6b93d287269a6e2f3d054788f65
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-12 13:55:58 -0300

    Initializing dark mode logic

--- src/base/dark-mode.js
+++ src/base/dark-mode.js
@@ -2,19 +2,35 @@ let activeDarkMode = document.getElementById('activeDarkMode')
 let activeLightMode = document.getElementById('activeLightMode')
 
 let pageBody = document.body
+let navbarDarkMode = document.querySelector('nav')
+let mainDarkMode = document.querySelector('main')
 
-function darkMode(body){
+function darkMode(){
     activeLightMode.style.display = 'block'
     activeDarkMode.style.display = 'none'
 
-    body.style.background = `#1C1C1C`
+    getColorForDarkMode(pageBody)
 }
 
-function lightMode(body){
+function lightMode(){
     activeLightMode.style.display = 'none'
     activeDarkMode.style.display = 'block'
 
+    removingColorFormDarkMode(pageBody)
+}
+
+function getColorForDarkMode(body){
+    body.style.background = `#1C1C1C`
+    
+    navbarDarkMode.classList.add('dark-mode')
+    mainDarkMode.classList.add('dark-mode')
+}
+
+function removingColorFormDarkMode(body){
     body.style.background = `#FFFFFF`
+
+    navbarDarkMode.classList.remove('dark-mode')
+    mainDarkMode.classList.remove('dark-mode')
 }
 
 activeDarkMode.addEventListener('click', () => {

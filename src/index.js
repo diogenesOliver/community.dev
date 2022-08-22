e83cdb869845e98c75f011fe507d97bdbb2dfd07
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-08-22 16:01:49 -0300

    MORE

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ let numberArticles = document.getElementById('numbers-articles')
 let communityDevSpan = document.getElementById('communityDevSpan')
 
 let typedUser = new Typed(numberUser, {
-    strings:['+ 500.000'],
+    strings:['500.000 +'],
     typeSpeed: 200,
     backSpeed: 90,
     loop: true
 })
 
 let typedCommunity = new Typed(numberCommunity, {
-    strings:['+ 100.000'],
+    strings:['100.000 +'],
     typeSpeed: 200,
     backSpeed: 90,
     loop: true
 })
 
 let typedArticles = new Typed(numberArticles, {
-    strings:['+ 800.000'],
+    strings:['800.000 +'],
     typeSpeed: 200,
     backSpeed: 90,
     loop: true
